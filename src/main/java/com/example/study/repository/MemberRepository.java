7b6f21e5fc28c7d172fd1c59f1c99b2ae2ecdcd6
kyoungmin <stephen4951@gmail.com>
2023-07-12 15:34:09 +0900

    # 3. QueryDsl 서브쿼리, 동적 SQL 활용법

--- src/main/java/com/example/study/repository/MemberRepository.java
+++ src/main/java/com/example/study/repository/MemberRepository.java
@@ -2,6 +2,15 @@ package com.example.study.repository;
 
 import com.example.study.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 }
+
+
+
+
+
+
+
+
